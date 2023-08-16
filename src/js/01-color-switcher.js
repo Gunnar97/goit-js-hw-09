6acdc9bb3e8f5407ada9a7d94b0ec99c5f5cc79b
Gunnar97 <135347097+Gunnar97@users.noreply.github.com>
2023-08-16 19:26:40 +0300

    add 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+    body: document.querySelector('body')
+}
+
+let delay = 1000;
+let timerId1;
+refs.startBtn.addEventListener('click', onClick);
+
+function onClick(eve) {
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+    
+    timerId1 = setInterval(() => refs.body.style.backgroundColor = getRandomHexColor(), delay);
+    function getRandomHexColor() {
+        return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+    };
+};
+
+refs.stopBtn.addEventListener('click', onStop);
+
+function onStop(eve) {
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+    clearInterval(timerId1);
+    
+};
+
+
+
+
+
 
